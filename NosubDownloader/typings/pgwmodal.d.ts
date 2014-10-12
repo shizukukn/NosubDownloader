5a6ce23c42e77cb60db5191de1a232587df61e2c
KONO Shizuku <shizukukn@gmail.com>
2014-10-12 14:03:20 +0900

    Add local typing

---
+++ NosubDownloader/typings/pgwmodal.d.ts
@@ -0,0 +1,26 @@
+﻿
+interface PgwModalOption {
+    content?: string;
+    target?: string;
+    url?: string;
+    title?: string;
+    titleBar?: boolean;
+    mainClassName?: string;
+    backdropClassName?: string;
+    maxWidth?: number;
+    angular?: boolean;
+    modalData?: any;
+    ajaxOptions?: any;
+    closable?: boolean;
+    closeContent?: string;
+    closeOnEscape?: boolean;
+    closeOnBackgroundClick?: boolean;
+    loadingContent?: string;
+    errorContent?: string;
+    pushContent?: string;
+}
+
+interface ZeptoStatic {
+    pgwModal(option: PgwModalOption): void;
+    pgwModal(action: string): void;
+}
