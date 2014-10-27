c0fcd74a5df4bd032c7083d3fc199d4483365603
KONO Shizuku <shizukukn@gmail.com>
2014-10-28 08:10:57 +0900

    Fix header

--- NosubDownloader/content_scripts/js/settings.ts
+++ NosubDownloader/content_scripts/js/settings.ts
@@ -1,4 +1,11 @@
-﻿
+﻿// Path: content_scripts/js/settings.js
+// License: GPLv3<http://www.gnu.org/licenses/>
+// Author: shizuku613<https://github.com/shizuku613>
+// Copyright (C) Shizuku Kono 2014
+
+/// <reference path="../../../typings/chrome/chrome.d.ts" />
+/// <reference path="../../global.d.ts" />
+
 module nosub.contentScripts.settings {
     function getSettingValue(name: string, cb: (value: string) => void): void {
         var message: SettingMessage = {
